08122bfa394f19f60878ba3b39f2ddac96447af3
ezzaffa88 <so1fien@hotmail.com>
2020-04-05 21:56:10 +0100

    deleted node_modules folder

---
+++ src/db/models/label/label.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+const labelSchema = mongoose.Schema({
+    id:{
+        type:Number,
+        required:true
+    },
+    handle:{
+        type:String
+    },
+    title:{
+        type:String
+    },
+    color:{
+        type:String
+    }
+})
+const label = mongoose.model('Label',labelSchema)
